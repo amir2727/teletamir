ad9c940912995e416ebd86e62c3902c59c91cf7d
amir2727 <amir.hossien2727@gmail.com>
2016-02-17 18:53:59 +0330

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -13,7 +13,7 @@
 ]]
 
 do
-local Arian = 80182995,132667916,119650184,156823206 --put your id here(BOT OWNER ID)
+local Amir = 119650184,000000,000000,000000 --put your id here(BOT OWNER ID)
 
 local function setrank(msg, name, value) -- setrank function
   local hash = nil
@@ -96,7 +96,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'shield Team'
+  text = text..'teletamirbot'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
@@ -135,7 +135,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'shield Team'
+  text = text..'teletamirbot'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
