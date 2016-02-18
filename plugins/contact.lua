d59939a22c1daf66fdd93823bb7cc966b5933f97
amir2727 <amir.hossien2727@gmail.com>
2016-02-18 20:09:19 +0330

    Create contact.lua

---
+++ plugins/contact.lua
@@ -0,0 +1,23 @@
+do
+
+function run(msg, matches)
+  return [[
+سازنده و مدیر کل ربات:
+ αмιя.м.η: @amirho3ein911
+
+  لیست ادمین ها
+
+ 
+    …………………………
+
+
+]]
+end
+
+return {
+  description = "tamir",
+  patterns = {"^[!/%$+=.-*&][Cc]ontact"},
+  run = run 
+}
+
+end
